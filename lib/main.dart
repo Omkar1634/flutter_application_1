f968d6bf84277c4dede12affc408a1fd39d7ab08
omkar kharkar <ok22160@essex.ac.uk>
2023-05-20 16:39:15 +0100

    day4 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(myApp());
@@ -11,11 +12,11 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark
-        ),
-        routes: {
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryTextTheme: GoogleFonts.latoTextTheme()),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage()
